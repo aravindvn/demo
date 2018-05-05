a5f868307af920d3b51c462e3b9e57ab4a8afe12
aravindvn <dasan.aravind@gmail.com>
2018-05-05 15:19:01 -0500

    development string added

--- src/main/java/com/example/demo/WelcomeController.java
+++ src/main/java/com/example/demo/WelcomeController.java
@@ -15,7 +15,8 @@ public class WelcomeController {
     private String stash2 ="This is stash 2 ";
 
 
-
+    private String development ="This is a string added at local to merge to remote";
+    
     @GetMapping("/welcome")
     public String welcome()
 
