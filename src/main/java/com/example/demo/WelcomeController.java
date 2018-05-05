0f67890c4a26f17752035dbac9a68f1eaedd5509
aravindvn <dasan.aravind@gmail.com>
2018-05-05 12:22:09 -0500

    SDN for Welcomecontroller

--- src/main/java/com/example/demo/WelcomeController.java
+++ src/main/java/com/example/demo/WelcomeController.java
@@ -8,6 +8,8 @@ public class WelcomeController {
 
     private String message = "hello my  world ";
 
+    private String SDN = "new SDN";ls -la
+
 
     @GetMapping("/welcome")
     public String welcome()
